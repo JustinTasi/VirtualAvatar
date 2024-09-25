d6cc37004f31c6f32d7b08b927fab65fcc68742f
ChengHan <wow0422796353@gmail.com>
2024-09-25 18:05:41 +0800

    修正 - strictmode寫法

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { StrictMode } from 'react';
 import './index.css'
 import { ChatProvider } from './hooks/useChat'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <ChatProvider>
       <App/>
     </ChatProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 )

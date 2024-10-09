d0bfade2e82167adabb48b74fad4b265440e7c73
ChengHan <wow0422796353@gmail.com>
2024-10-09 12:19:55 +0800

    新增 - 傳輸身體不適之API串接

--- services/OpenAIAPI.jsx
+++ services/OpenAIAPI.jsx
@@ -4,6 +4,9 @@ const BASE_URL = "medicament/api"
 const openAIAPI = {
   'sendMessageToOpenAi': (data) =>
     API.postAPI(`${BASE_URL}/openAi`, data),
+
+  'sendUncomfortableMessage': (data) =>
+    API.postAPI(`${BASE_URL}/openAi`, data),
 }
 
 export default openAIAPI;

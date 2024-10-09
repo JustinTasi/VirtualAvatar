a9ed66d79aabc23e171460071776b5a5bbd08270
ChengHan <wow0422796353@gmail.com>
2024-10-09 12:19:24 +0800

    新增 - provider於main

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,21 @@ import App from './App'
 import { StrictMode } from 'react';
 import './css/index.css'
 import { ChatProvider } from './hooks/useChat'
+import { AuthProvider } from './hooks/useAuth'
+import { LoadingProvider } from './hooks/useLoading'
+import { MessageModalProvider } from './hooks/useMessageModal'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChatProvider>
-      <App/>
-    </ChatProvider>
-  </StrictMode>,
+    <LoadingProvider>
+      <MessageModalProvider>
+        <AuthProvider>
+          <ChatProvider>
+            <App/>
+          </ChatProvider>
+        </AuthProvider>
+      </MessageModalProvider>
+    </LoadingProvider>
+  </StrictMode>
 )

b978d7e3c749058092da49a9800c9c5b74a10a3e
ChengHan <wow0422796353@gmail.com>
2024-10-14 20:48:25 +0800

    修改 - route路徑

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AccessDeny from "./pages/AccessDeny";
-import AvatarScene from './pages/AvatarScene';
+import FemaleDoctor1 from './pages/FemaleDoctor1';
+import FemaleDoctor2 from './pages/FemaleDoctor2';
+import MaleDoctor1 from './pages/MaleDoctor1';
+import MaleDoctor2 from './pages/MaleDoctor2';
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/accessDeny" element={<AccessDeny/>} />
-        <Route path="/" element={<AvatarScene/>} />
+        <Route path="/female1" element={<FemaleDoctor1/>} />
+        <Route path="/female2" element={<FemaleDoctor2/>} />
+        <Route path="/male1" element={<MaleDoctor1/>} />
+        <Route path="/male2" element={<MaleDoctor2/>} />
       </Routes>
     </>
   );
